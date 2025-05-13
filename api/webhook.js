68630ba5afce49eae1290187e84b36b4c356a546
RaphaelCabigas <raph.ccc3@gmail.com>
2025-05-13 13:59:27 +0400

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
     console.log(params);
 
     // * Assign the corresponding parameters used
-    const name = params['given-name'];
+    const name = params['custom-name'];
     const email = params['email'];
     const allowedTime = params['allowed-time'];
     const allowedGuest = params['allowed-guests'];
